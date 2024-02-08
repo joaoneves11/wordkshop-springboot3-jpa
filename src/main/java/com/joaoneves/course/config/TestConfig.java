f7d3d4f525bd42e2c72a9e1111f07356c9869f50
João Neves <joaopedro@outlook.com.br>
2024-02-08 16:55:20 -0300

    payment, one-to-one association

--- src/main/java/com/joaoneves/course/config/TestConfig.java
+++ src/main/java/com/joaoneves/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.joaoneves.course.entities.Category;
 import com.joaoneves.course.entities.Order;
 import com.joaoneves.course.entities.OrderItem;
+import com.joaoneves.course.entities.Payment;
 import com.joaoneves.course.entities.Product;
 import com.joaoneves.course.entities.User;
 import com.joaoneves.course.entities.enums.OrderStatus;
@@ -82,6 +83,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		//para classes dependentes é dessa forma que se salva no banco de dados
 		
 		}
 	
